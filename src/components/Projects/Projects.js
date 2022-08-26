c2f80b29b024913bdcea76f6379f87b690c0f5b9
ninjadev0706 <103828922+ninjadev0706@users.noreply.github.com>
2022-08-26 02:41:35 +0200

    changed projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,7 +7,7 @@ import project2 from "../../Assets/Projects/project2.png";
 import project3 from "../../Assets/Projects/project3.png";
 import project4 from "../../Assets/Projects/project4.png";
 import project5 from "../../Assets/Projects/project5.png";
-// import project6 from "../../Assets/Projects/project6.png";
+import project6 from "../../Assets/Projects/project6.png";
 
 function Projects() {
   return (
@@ -25,10 +25,10 @@ function Projects() {
             <ProjectCard
               imgPath={project1}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+              title="https://www.terraspaces.io/"
+              description="Near protocal's marketplace. I developed the smart contract with Rust. And built the website with Next.js and integrated with smart contract using near-api-js."
               ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              demoLink="https://www.terraspaces.io/"
             />
           </Col>
 
@@ -36,10 +36,10 @@ function Projects() {
             <ProjectCard
               imgPath={project2}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
+              title="https://100days.finance/"
+              description="IDO launchpad for astro token. Astro token is rebase one. I developed the smart contract and exchange using traderjoe sdk. And developed the front end with React."
               ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              demoLink="https://100days.finance/"
             />
           </Col>
 
@@ -47,10 +47,9 @@ function Projects() {
             <ProjectCard
               imgPath={project3}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="https://app.croxswap.com/"
+              description="Next-Gen Staking & Dual Farming Protocal. Build the website with TypeScript and developed the farm, pool, Dual Farm, Grand pool, ICO, Vpot, etc."
+              demoLink="https://app.croxswap.com/"              
             />
           </Col>
 
@@ -58,10 +57,9 @@ function Projects() {
             <ProjectCard
               imgPath={project4}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="https://www.privi.store/"
+              description="New Age decentralized app store. Developed the front end with React and built the smart contract, integrated with ethers.js"
+              demoLink="https://www.privi.store/"
             />
           </Col>
 
@@ -69,22 +67,19 @@ function Projects() {
             <ProjectCard
               imgPath={project5}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              title="https://mint.marketofzion.com/"
+              description="NFT minting website on Fantom. Created and deployed the smart contract to Fantom. And built the website with wallet integration via react."
+              demoLink="https://mint.marketofzion.com/" 
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={project5}
+              imgPath={project6}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              title="https://the-devil-assassin.vercel.app/"
+              description="Solana NFT minting website. Built the website with wallet integration via react and web3.js. Developed the smart contract at Solana."
+              demoLink="https://the-devil-assassin.vercel.app/" 
             />
           </Col>
         </Row>
